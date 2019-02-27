4819e0d54582ec60493c0a830a2c646fb3bb0865
gleamora <daitiangao38@outlook.com>
2019-02-27 17:27:13 +0800

    update

--- test/app/config/routes.rb
+++ test/app/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to=> 'welcome#index', :as => 'home'
+  match 'login', :to => 'account#login', :as => 'signin', :via =>[:get]
 end
